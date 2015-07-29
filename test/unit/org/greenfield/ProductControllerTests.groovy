1c85c78892e6fd55059f49533d7990ce1a2860eb
Mike <croteau.mike@gmail.com>
2015-07-28 20:15:42 -0800

    completed first pass at test cleanup

--- test/unit/org/greenfield/ProductControllerTests.groovy
+++ test/unit/org/greenfield/ProductControllerTests.groovy
@@ -9,6 +9,8 @@ import grails.test.mixin.*
 @Mock(Product)
 class ProductControllerTests {
 
+	//TODO : add tests
+	
     def populateValidParams(params) {
         assert params != null
         // TODO: Populate valid properties like...
@@ -17,139 +19,13 @@ class ProductControllerTests {
 
     void testIndex() {
         controller.index()
-        assert "/product/list" == response.redirectedUrl
+        assert flash.message == null
     }
+	
 
-    void testList() {
-
-        def model = controller.list()
-
-        assert model.productInstanceList.size() == 0
-        assert model.productInstanceTotal == 0
+    void testSearch() {
+        controller.search()
+        assert flash.message == "Search query must be at least 4 characters"
     }
-
-    void testCreate() {
-        def model = controller.create()
-
-        assert model.productInstance != null
-    }
-
-    void testSave() {
-        controller.save()
-
-        assert model.productInstance != null
-        assert view == '/product/create'
-
-        response.reset()
-
-        populateValidParams(params)
-        controller.save()
-
-        assert response.redirectedUrl == '/product/show/1'
-        assert controller.flash.message != null
-        assert Product.count() == 1
-    }
-
-    void testShow() {
-        controller.show()
-
-        assert flash.message != null
-        assert response.redirectedUrl == '/product/list'
-
-        populateValidParams(params)
-        def product = new Product(params)
-
-        assert product.save() != null
-
-        params.id = product.id
-
-        def model = controller.show()
-
-        assert model.productInstance == product
-    }
-
-    void testEdit() {
-        controller.edit()
-
-        assert flash.message != null
-        assert response.redirectedUrl == '/product/list'
-
-        populateValidParams(params)
-        def product = new Product(params)
-
-        assert product.save() != null
-
-        params.id = product.id
-
-        def model = controller.edit()
-
-        assert model.productInstance == product
-    }
-
-    void testUpdate() {
-        controller.update()
-
-        assert flash.message != null
-        assert response.redirectedUrl == '/product/list'
-
-        response.reset()
-
-        populateValidParams(params)
-        def product = new Product(params)
-
-        assert product.save() != null
-
-        // test invalid parameters in update
-        params.id = product.id
-        //TODO: add invalid values to params object
-
-        controller.update()
-
-        assert view == "/product/edit"
-        assert model.productInstance != null
-
-        product.clearErrors()
-
-        populateValidParams(params)
-        controller.update()
-
-        assert response.redirectedUrl == "/product/show/$product.id"
-        assert flash.message != null
-
-        //test outdated version number
-        response.reset()
-        product.clearErrors()
-
-        populateValidParams(params)
-        params.id = product.id
-        params.version = -1
-        controller.update()
-
-        assert view == "/product/edit"
-        assert model.productInstance != null
-        assert model.productInstance.errors.getFieldError('version')
-        assert flash.message != null
-    }
-
-    void testDelete() {
-        controller.delete()
-        assert flash.message != null
-        assert response.redirectedUrl == '/product/list'
-
-        response.reset()
-
-        populateValidParams(params)
-        def product = new Product(params)
-
-        assert product.save() != null
-        assert Product.count() == 1
-
-        params.id = product.id
-
-        controller.delete()
-
-        assert Product.count() == 0
-        assert Product.get(product.id) == null
-        assert response.redirectedUrl == '/product/list'
-    }
+    
 }
