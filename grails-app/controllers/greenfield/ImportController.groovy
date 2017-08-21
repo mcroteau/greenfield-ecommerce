5c6be0441cd0c131c83e1c5d342b82b38a848e4b
Mike Croteau <croteau.mike@gmail.com>
2017-08-21 15:47:44 -0800

    working through data import

--- grails-app/controllers/greenfield/ImportController.groovy
+++ grails-app/controllers/greenfield/ImportController.groovy
@@ -7,7 +7,30 @@ import org.greenfield.Account
 import org.greenfield.Permission
 import org.greenfield.Catalog
 import org.greenfield.Product
+import org.greenfield.ProductOption
+import org.greenfield.Variant
+
+import org.greenfield.Specification
+import org.greenfield.SpecificationOption
+import org.greenfield.ProductSpecification
+
+import org.greenfield.AdditionalPhoto
+
+import org.greenfield.ShoppingCart
+import org.greenfield.ShoppingCartItem
+import org.greenfield.ShoppingCartItemOption
+
+import org.greenfield.Transaction
+import org.greenfield.Page
+import org.greenfield.Upload
+import org.greenfield.Layout
+
+import org.greenfield.log.CatalogViewLog
+import org.greenfield.log.PageViewLog
+import org.greenfield.log.ProductViewLog
+import org.greenfield.log.SearchLog
 import org.greenfield.State
+
 
 import java.io.File
 import groovy.json.JsonSlurper
@@ -17,6 +40,8 @@ import grails.converters.JSON
 
 @Mixin(BaseController)
 class ImportController {
+	
+	def catalogCount = 0
 	
 	@Secured(['ROLE_ADMIN'])
 	def parse(){
@@ -43,41 +68,102 @@ class ImportController {
  	@Secured(['ROLE_ADMIN'])
 	def import_data(){
 			
-		def jsonMultipartFile = request.getFile('json-data')
-		def jsonFile = convert(jsonMultipartFile)
 		
-		def jsonSlurper = new JsonSlurper()
-		def json = jsonSlurper.parseText(jsonFile.text)
+		try{
+			def jsonMultipartFile = request.getFile('json-data')
 		
-		if(json['accounts']){
-			saveAccountData(json['accounts'])
+		
+			def jsonFile = convert(jsonMultipartFile)
+			
+			def jsonSlurper = new JsonSlurper()
+			def json = jsonSlurper.parseText(jsonFile.text)
+			
+			if(json['accounts']){
+				saveAccountData(json['accounts'])
+			}
+			
+			if(json['catalogs']){
+				saveCatalogData(json['catalogs'])
+			}
+			
+			if(json['productOptionData']){
+				saveProductOptionData(json['productOptionData'])
+			}
+			
+			if(json['products']){
+				saveProductData(json['products'])
+			}
+			
+			render(view: 'view_import')
+			
+		}catch(IOException ioe){
+			flash.message = "No file was selected. Please select a file to continue"
+			redirect(action: 'view_import')
+		}catch(Exception e){
+			flash.message = "Something went wrong, please try again"
+			redirect(action: 'view_import')
 		}
-		
-		/** TODO:unnecessary?
-		if(json['permissions']){
-			savePermissionData(json['permissions'])
-		}
-		**/
-		
-		if(json['catalogs']){
-			saveCatalogData(json['catalogs'])
-		}
-		
-		if(json['products']){
-			saveProductData(json['products'])
-		}
-		
-		render(view : 'view_import')
 	}
 	
 	
+	def saveProductOptionData(productOptionData){
+		def count = 0
+		if(productOptionData.productOptions){
+			println "here..."
+			productOptionData.productOptions.each(){ data ->
+				def product = Product.findByUuid(data.product)
+				if(product){
+					println "product = " + product
+					if(params.performImport == "true"){		
+						def productOption = new ProductOption()
+						productOption.uuid = data.uuid
+						productOption.product = product
+						productOption.save(flush:true)
+						
+						product.addToProductOptions(productOption)
+						product.save(flush:true)
+					}
+					count++
+				}
+			}
+			
+			if(productOptionData.optionVariants){
+				productOptionData.optionVariants.each(){ datac ->
+					def productOption = ProductOption.findByUuid(datac.productOption)
+					
+					if(productOption){	
+						def variant = new Variant()
+						variant.uuid = datac.uuid
+						variant.name = datac.name
+						variant.price = datac.price
+						variant.imageUrl = datac.imageUrl
+						variant.position = datac.position
+						variant.productOption = productOption
+
+						if(params.performImport == "true"){	
+							variant.save(flush:true)
+							productOption.addToVariants(variant)
+							productOption.save(flush:true)
+						}
+					}
+				}
+			}
+		}
+		
+		request.productOptionsCount = count
+	}
+	
+	
+	
 	def saveProductData(products){
+		def count = 0
+		
 		products.each(){ data ->
+			def existingProduct = Product.findByUuid(data.uuid)
 			
-			def existingProduct = Product.findByName(data.name)
-			
-			if(!existingProduct){
+			if(!existingProduct && data.catalogs){
 				def product = new Product()
+				product.uuid = data.uuid
 				product.name = data.name
 				product.description = data.description
 				product.quantity = data.quantity
@@ -94,26 +180,28 @@ class ImportController {
 				product.dateCreated = Date.parse("yyyy-MM-dd'T'HH:mm:ssX", data.dateCreated)
 		    	product.lastUpdated = Date.parse("yyyy-MM-dd'T'HH:mm:ssX", data.lastUpdated)
 						
-				product.save(flush:true)
-					
-				existingProduct = product
-			}
-			
-			if(data.catalogs){
-				data.catalogs.each(){ datax ->
-					def catalog = Catalog.findByName(datax)
-					if(catalog){
-						existingProduct.addToCatalogs(catalog)
-						existingProduct.save(flush:true)
+				if(params.performImport == "true"){		
+					product.save(flush:true)
+			    	
+					data.catalogs.each(){ c ->
+						def catalog = Catalog.findByUuid(c)
+						if(catalog){
+							product.addToCatalogs(catalog)
+							product.save(flush:true)
+						}
 					}
-
 				}
+				count++			
+			}else{
+				flash.message = "Make sure catalogs have been imported before products otherwise products will not be imported."
 			}
+			request.productsCount = count			
 		}
 	}
 	
 	
 	def saveAccountData(accounts){
+		def count = 0
 		accounts.each(){ data ->
 			if(data.username != 'admin'){
 			
@@ -122,7 +210,8 @@ class ImportController {
 				if(!existingAccount){
 					println data.username
 					def account = new Account()
-					//account.id = data.id
+					
+					account.uuid = data.uuid
 					account.username = data.username
 					account.password = data.password
 					account.name = data.name
@@ -144,36 +233,23 @@ class ImportController {
 		            account.dateCreated = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'", data.dateCreated)
 		            account.lastUpdated = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'", data.lastUpdated)
 					
-					account.save(flush:true)
+					if(params.performImport == "true"){
+						account.save(flush:true)
 					
-					if(account.hasAdminRole){
-						account.createAccountRoles(true)
+						if(account.hasAdminRole){
+							account.createAccountRoles(true)
+						}else{
+							account.createAccountRoles(false)
+						}
+						account.createAccountPermission()
+						
+						request.importResults = "show"
 					}else{
-						account.createAccountRoles(false)
+						request.checkResults = "show"
 					}
-					account.createAccountPermission()
-				}
-			}
-		}
-	}
-	
-
-	
-	
-	def savePermissionData(permissions){
-		permissions.each(){ data ->
-			if(data['account'] && data['permission']){
-				def account = Account.findByUsername(data['account'])
-				
-				if(account){
-					def existingPermission = Permission.findByAccountAndPermission(account, data['permission'])
 					
-					if(!existingPermission){
-						def permission = new Permission()
-						permission.account = account
-						permission.permission = data['permission']
-						permission.save(flush:true)
-					}
+					count++
+					request.accountsCount = count
 				}
 			}
 		}
@@ -181,40 +257,47 @@ class ImportController {
 	
 	
 	def saveCatalogData(catalogs){
+		catalogCount = 0
 		catalogs.each(){ data ->
-			def existingCatalog = Catalog.findByName(data['name'])
+			def existingCatalog = Catalog.findByUuid(data.uuid)
 			
 			if(!existingCatalog){
 				def catalog =  populateCatalogData(data)
-				catalog.toplevel = true
-				catalog.save(flush:true)
+				
+				if(params.performImport == "true"){
+					catalog.toplevel = true
+					catalog.save(flush:true)
+				}
 
-				existingCatalog = catalog
-			}
-			if(data['subcatalogs']){
-				saveSubcatalogs(existingCatalog, data['subcatalogs'])
+				if(data.subcatalogs){
+					saveSubcatalogs(catalog, data.subcatalogs)
+				}
+				catalogCount++
 			}
 		}
+		request.catalogsCount = catalogCount
 	}
 	
 	
 	def saveSubcatalogs(parentCatalog, subcatalogs){
 		subcatalogs.each(){ data ->
-			def existingCatalog = Catalog.findByName(data['name'])
+			def existingCatalog = Catalog.findByUuid(data.uuid)
 			
 			if(!existingCatalog){
 				def catalog = populateCatalogData(data)
-				catalog.toplevel = false
-				catalog.save(flush:true)
 				
-				parentCatalog.addToSubcatalogs(catalog)
-				parentCatalog.save(flush:true)
-				
-				existingCatalog = catalog
-			}
-			println "data = " + data
-			if(data['subcatalogs']){
-				saveSubcatalogs(existingCatalog, data['subcatalogs'])
+				if(params.performImport == "true"){
+					catalog.toplevel = false
+					catalog.save(flush:true)
+					
+					parentCatalog.addToSubcatalogs(catalog)
+					parentCatalog.save(flush:true)
+					
+				}
+				if(data.subcatalogs){
+					saveSubcatalogs(existingCatalog, data.subcatalogs)
+				}
+				catalogCount++
 			}
 		}
 	}
@@ -222,10 +305,11 @@ class ImportController {
 	
 	def populateCatalogData(data){
 		def catalog = new Catalog()
-		catalog.name = data['name']
-		catalog.description = data['description'] 
-		catalog.position = data['position']
-		def parentCatalog = Catalog.findByName(data['parentCatalog'])
+		catalog.uuid = data.uuid
+		catalog.name = data.name
+		catalog.description = data.description
+		catalog.position = data.position
+		def parentCatalog = Catalog.findByUuid(data.parentCatalog)
 		if(parentCatalog){
 			catalog.parentCatalog = parentCatalog
 		}
--- grails-app/views/import/view_import.gsp
+++ grails-app/views/import/view_import.gsp
@@ -7,18 +7,112 @@
 	<body>
 		
 		<div id="export-data-view" class="content" role="main">
+	
+			<g:if test="${flash.message}">
+				<div class="alert alert-info" role="status">${flash.message}</div>
+			</g:if>
 		
-			<h2>Import Data</h2>
-			<p class="secondary">Expecting Greenfield Json formatted data</p>
-			<p class="secondary">* Accounts will by default be set with role of Customer only</p>
-			<p class="secondary">* Import & Export is meant as an alternate backup mechanism of Greenfield data</p>
-
-			<g:uploadForm action="import_data" class="form-horizontal" >
-				<input type="file" name="json-data" id="json-data" />
-				<br/>
-				<g:submitButton name="import" class="btn btn-primary" value="Import Data"/>
-			</g:uploadForm>
-		
+			<div id="data-check-container">
+				
+				<div id="data-check-form">
+					<h2 style="margin:10px 0px; padding:0px;">Data Check</h2>
+					<p class="secondary">Import &amp; Export is meant as an alternate backup mechanism of data<br/>
+					No Data will be imported, this will just display data to be imported<br/>
+					Expecting Greenfield Json formatted data</p>
+            		
+            		
+					<g:uploadForm action="import_data" class="form-horizontal" >
+						<input type="file" name="json-data" id="json-data" />
+						<br/>
+						<g:submitButton name="import" class="btn btn-primary" value="Perform Import Data Check"/>		
+					</g:uploadForm>
+			
+			
+			
+					<g:if test="${!checkResults || checkResults}">
+			
+						<h4 class="pull-left">Results Data Check </h4>
+			
+						<p class="secondary pull-left" style="margin:10px 10px">- Data to be imported on "Import Data"</p>
+						
+						<table class="table">
+							<tr>
+								<td><strong>${accountsCount ? accountsCount : 0}</strong>&nbsp;:&nbsp;Accounts</td>
+								<td><strong>${catalogsCount ? catalogsCount : 0}</strong>&nbsp;:&nbsp;Catalogs</td>
+								<td><strong>${productsCount ? productsCount : 0}</strong>&nbsp;:&nbsp;Products</td>
+							</tr>
+							
+							<tr>
+								<td><strong>${productOptionsCount ? productOptionsCount : 0}</strong>&nbsp;:&nbsp;Product Options</td>
+								<td><strong>${productSpecificationsCount ? productSpecificationsCount : 0}</strong>&nbsp;:&nbsp;Product Specifications</td>
+								<td><strong>${additionalPhotosCount ? additionalPhotosCount : 0}</strong>&nbsp;:&nbsp;Additional Photos</td>
+							</tr>
+							<tr>
+								<td><strong>${shoppingCartsCount ? shoppingCartsCount : 0}</strong>&nbsp;:&nbsp;Shopping Carts</td>
+								<td><strong>${ordersCount ? ordersCount : 0}</strong>&nbsp;:&nbsp;Orders</td>
+								<td><strong>${pagesCount ? pagesCount : 0}</strong>&nbsp;:&nbsp;Pages</td>
+							</tr> 
+							<tr>
+								<td><strong>${uploadsCount ? uploadsCount : 0}</strong>&nbsp;:&nbsp;Uploads</td>
+								<td><strong>${layoutsCount ? layoutsCount : 0}</strong>&nbsp;:&nbsp;Layout</td>
+								<td><strong>${logsCount ? logsCount : 0}</strong>&nbsp;:&nbsp;Logs</td>
+							</tr>
+						</table>
+					</g:if>
+				</div>
+			</div>
+			
+			<br class="clear"/>
+			<br/>
+	
+			<div id="data-check-container">
+			
+				<div id="data-check-form" style="margin-top:0px;">
+					<h2 style="margin-top:0px">Import Data</h2>
+            		
+					<p class="secondary">This will perform data import<br/>
+					Expecting Greenfield Json formatted data. <br/>Run 'Data Check' to insure data to be imported is correct</p>
+					
+					<g:uploadForm action="import_data" class="form-horizontal" >
+						
+						<input type="hidden" name="performImport" value="true"/>
+						<input type="file" name="json-data" id="json-data" />
+						
+						<br/>
+						<g:submitButton name="import" class="btn btn-primary" value="Perform Import Data"/>
+					</g:uploadForm>
+					
+					
+					<g:if test="${!importResults || importResults}">
+						<h4 class="pull-left">Data Import Results</h4>
+						<p class="secondary pull-left" style="margin:10px 10px;">- Data imported</p>
+						
+						<table class="table">
+							<tr>
+								<td><strong>${accountsCount ? accountsCount : 0}</strong>&nbsp;:&nbsp;Accounts</td>
+								<td><strong>${catalogsCount ? catalogsCount : 0}</strong>&nbsp;:&nbsp;Catalogs</td>
+								<td><strong>${productsCount ? productsCount : 0}</strong>&nbsp;:&nbsp;Products</td>
+							</tr>
+							
+							<tr>
+								<td><strong>${productOptionsCount ? productOptionsCount : 0}</strong>&nbsp;:&nbsp;Product Options</td>
+								<td><strong>${productSpecificationsCount ? productSpecificationsCount : 0}</strong>&nbsp;:&nbsp;Product Specifications</td>
+								<td><strong>${additionalPhotosCount ? additionalPhotosCount : 0}</strong>&nbsp;:&nbsp;Additional Photos</td>
+							</tr>
+							<tr>
+								<td><strong>${shoppingCartsCount ? shoppingCartsCount : 0}</strong>&nbsp;:&nbsp;Shopping Carts</td>
+								<td><strong>${ordersCount ? ordersCount : 0}</strong>&nbsp;:&nbsp;Orders</td>
+								<td><strong>${pagesCount ? pagesCount : 0}</strong>&nbsp;:&nbsp;Pages</td>
+							</tr> 
+							<tr>
+								<td><strong>${uploadsCount ? uploadsCount : 0}</strong>&nbsp;:&nbsp;Uploads</td>
+								<td><strong>${layoutsCount ? layoutsCount : 0}</strong>&nbsp;:&nbsp;Layout</td>
+								<td><strong>${logsCount ? logsCount : 0}</strong>&nbsp;:&nbsp;Logs</td>
+							</tr>
+						</table>
+					</g:if>
+				</div>
+			</div>
 		</div>
 		
 	</body>
